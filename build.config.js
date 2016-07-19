46531d09e49f88bebddc5a641879019a9bd67d97
Lilia Hristova <lilia.hristova@softwaregroup-bg.com>
2016-07-19 17:06:34 +0300

    Минифициране на js-a
    Конфиг за минифициране на js-a с r.js (той трябва отделно да се
    инсталира) и първия минифициран файл на js-a.

---
+++ build.config.js
@@ -0,0 +1,23 @@
+﻿({
+    uglify2: {
+        mangle: true
+    },
+    name: "require.config",
+    baseUrl: ".",
+    mainConfigFile: "require.config.js",
+    out: "app.min.js",
+    generateSourceMaps: false,
+    preserveLicenseComments: false,
+    wrap: true,
+    optimize: "uglify2"
+    //optimize: "none"
+
+    /*dir: "../app-dist",
+    modules: [
+      {
+        name: "app/app.js"
+      }
+    ]*/
+    //sudo r.js -o build.config.js
+    //r.js.cmd -o build.config.js
+})
